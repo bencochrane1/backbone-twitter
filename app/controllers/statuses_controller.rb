9f4c5f5b06e583f02a8db6f4541f2775c1ef2f19
Ben Cochrane <bencochrane1@me.com>
2015-01-20 10:30:53 +1100

    remove handlebars cdn and script tag used as an example

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -6,4 +6,33 @@ class StatusesController < ApplicationController
             f.json { render json: @statuses }
         end
     end
+
+    def create
+        @status = Status.create(status_params)
+
+        render json: @status        
+    end
+
+    def update
+        @status = Status.find(params[:id])
+        @status.update(status_params)
+
+        render json: @status
+    end
+
+    def destroy
+        @status = Status.find(params[:id])
+        @status.destroy
+
+        render json: { status: "ok" }
+    end
+
+
+    private
+
+    def status_params
+        params.require(:status).permit(:username, :content)
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+
   resources :statuses
+
+  root 'pages#home'
 end
